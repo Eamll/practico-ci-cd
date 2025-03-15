78d396ae03957605bf296086f2520e5a91cb9351
chris dennys <chrisdennys04@gmail.com>
2025-03-15 06:34:34 -0400

    definicion del modelo y creacion del factory de empleado

--- app/Models/Empleado.php
+++ app/Models/Empleado.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Empleado extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nombres',
+        'apellidos',
+        'puesto',
+        'fecha_contratacion',
+        'salario',
+        'email',
+        'telefono',
+        'departamento',
+    ];
 }
